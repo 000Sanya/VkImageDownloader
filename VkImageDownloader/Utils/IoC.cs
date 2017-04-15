0eb656c22bf31245fc354293b7a47eafe67893af
NullSanya <000sanya.000sanya@gmail.com>
2017-04-15 12:42:32 +1000

    Добавлен IoC

---
+++ VkImageDownloader/Utils/IoC.cs
@@ -0,0 +1,27 @@
+﻿using Ninject;
+using Ninject.Modules;
+using NLog;
+
+namespace VkImageDownloader.Utils
+{
+	public class IoC
+	{
+		private static IKernel _kernel;
+		public static IKernel Kernel => _kernel ?? (_kernel = new StandardKernel());
+
+		public static void AddModule(INinjectModule module)
+		{
+			Kernel.Load(module);
+		}
+
+		public static T Get<T>()
+		{
+			return (T) Kernel.Get(typeof(T));
+		}
+
+		public static Logger GetLogger(string name)
+		{
+			return LogManager.GetLogger(name);
+		}
+	}
+}
